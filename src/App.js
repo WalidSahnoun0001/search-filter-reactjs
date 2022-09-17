dddbb4dd8d8ed105345d7a547928a6d4eac20481
Walid Sahnoun / وليد سحنون <77199554+WalidSahnoun0001@users.noreply.github.com>
2022-09-17 08:56:51 +0100

    Search Filter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import data from './names.json'
 
 function App() {
+
+  const [name, setName] = useState("")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='display'>
+        <input type="text" name="screen" placeholder='Enter a name...' onChange={(e)=> setName(e.target.value)}/>
+      </div>
+      <div>
+        <ul>
+          {
+            data.filter((value) => {
+              if(name === "") {
+                return value
+              } else if(value.name.trim().toLowerCase().includes(name.trim().toLowerCase())) {
+                return value
+              }
+            }).map((name)=> (
+              <li key={name.id}>{name.name}</li>
+            ))
+          }
+        </ul>
+      </div>
     </div>
   );
 }
